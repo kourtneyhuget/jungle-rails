e3bb4b55b6e9b67010856479086a8b686e5578e7
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-11-11 19:30:31 +0000

    create dashboard for product and category table with counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
   def show
+    @product_count = Product.count
+
+    @category_list = Product.group(:category_id).count
   end
 end
