ae49170f866fd50bf9321c2b647c918830f033ed
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-11-17 22:03:29 +0000

    product details feature spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,30 +1,30 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+# RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
-  # SETUP
-  before :each do
-    @category = Category.create! name: 'Apparel'
+#   # SETUP
+#   before :each do
+#     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
-      @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
-        description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
-        quantity: 10,
-        price: 64.99
-      )
-    end
-  end
+#     10.times do |n|
+#       @category.products.create!(
+#         name:  Faker::Hipster.sentence(3),
+#         description: Faker::Hipster.paragraph(4),
+#         image: open_asset('apparel1.jpg'),
+#         quantity: 10,
+#         price: 64.99
+#       )
+#     end
+#   end
 
-  scenario "They see all products" do
-    # ACT
-    visit root_path
+#   scenario "They see all products" do
+#     # ACT
+#     visit root_path
 
-    # DEBUG
-    save_screenshot
+#     # DEBUG
+#     save_screenshot
 
-    # VERIFY
-    expect(page).to have_css 'article.product', count: 10
-  end
-end
+#     # VERIFY
+#     expect(page).to have_css 'article.product', count: 10
+#   end
+# end
---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor goes from home page to product page by clicking on a product", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+  end
+
+  scenario "They see all products" do
+    visit root_path
+
+    first("article.product").find_link('Details').click
+    expect(page).to have_css '.products-show'
+    save_screenshot
+  end
+end
