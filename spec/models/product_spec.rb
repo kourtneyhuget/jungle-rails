3622488d812a1f120f91dc60a254a50897be070f
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-11-17 20:48:17 +0000

    uncomment product_spec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,40 +1,40 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Product, type: :model do
-#   describe 'Validations' do
-#     it "should save successfully" do
-#       @category = Category.create(name: 'Clothing')
-#       @product = @category.products.create(name: 'shoes', description: 'test', image: 'test', price_cents: 20000, quantity: 2)
-#       expect(@product).to be_valid
-#       @product.errors.full_messages
-#     end
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it "should save successfully" do
+      @category = Category.create(name: 'Clothing')
+      @product = @category.products.create(name: 'shoes', description: 'test', image: 'test', price_cents: 20000, quantity: 2)
+      expect(@product).to be_valid
+      @product.errors.full_messages
+    end
 
-#     it "name presence" do
-#       @category = Category.create(name: 'Clothing')
-#       @product = @category.products.create(name: nil, description: 'test', image: 'test', price_cents: 20000, quantity: 2)
-#       expect(@product).not_to be_valid
-#       @product.errors.full_messages
-#     end
+    it "name presence" do
+      @category = Category.create(name: 'Clothing')
+      @product = @category.products.create(name: nil, description: 'test', image: 'test', price_cents: 20000, quantity: 2)
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
+    end
 
-#     it "price_cents presence" do
-#       @category = Category.create(name: 'Clothing')
-#       @product = @category.products.create(name: 'shoes', description: 'test', image: 'test', price_cents: nil, quantity: 2)
-#       expect(@product).not_to be_valid
-#       @product.errors.full_messages
-#     end
+    it "price_cents presence" do
+      @category = Category.create(name: 'Clothing')
+      @product = @category.products.create(name: 'shoes', description: 'test', image: 'test', price_cents: nil, quantity: 2)
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
+    end
 
-#     it "quantity presence" do
-#       @category = Category.create(name: 'Clothing')
-#       @product = @category.products.create(name: 'shoes', description: 'test', image: 'test', price_cents: 20000, quantity: nil)
-#       expect(@product).not_to be_valid
-#       @product.errors.full_messages
-#     end
+    it "quantity presence" do
+      @category = Category.create(name: 'Clothing')
+      @product = @category.products.create(name: 'shoes', description: 'test', image: 'test', price_cents: 20000, quantity: nil)
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
+    end
 
-#     it "category presence" do
-#       @product = Product.create(name: 'shoes', description: 'test', image: 'test', price_cents: 20000, quantity: 2)
-#       expect(@product).not_to be_valid
-#       @product.errors.full_messages
-#     end
-#   end
-# end
+    it "category presence" do
+      @product = Product.create(name: 'shoes', description: 'test', image: 'test', price_cents: 20000, quantity: 2)
+      expect(@product).not_to be_valid
+      @product.errors.full_messages
+    end
+  end
+end
 
